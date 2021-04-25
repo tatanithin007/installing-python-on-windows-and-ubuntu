6cf5b7f7c69b73f246e25d8c88f82f8ae6b1d473
tatanithin007 <tatanithin007@gmail.com>
2021-04-25 07:02:01 +0530

    added unrestricted
    added unrestricted

--- autocontroll.ps1
+++ autocontroll.ps1
@@ -1,7 +1,8 @@
 #@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 #for windows, first change the device name in config.json before executing it.
 #this is a powershell file
-Set-ExecutionPolicy unrestricted
+#Set-ExecutionPolicy unrestricted
+Set-ExecutionPolicy Bypass
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y python3
 #py -v
@@ -16,3 +17,4 @@ Copy-Item "getdatafromapi.ps1" -Destination "C:\API"
 Copy-Item "trackpyjob.ps1" -Destination "C:\API"
 Register-ScheduledTask -xml (Get-Content 'getdatafromapi.xml' | Out-String) -TaskName "getdatafromapi" -TaskPath "\MyTasks" -User $env:userdomain\$env:username -Force
 Register-ScheduledTask -xml (Get-Content 'trackpyjob.xml' | Out-String) -TaskName "trackpyjob" -TaskPath "\MyTasks" -User $env:userdomain\$env:username -Force
+Set-ExecutionPolicy unrestricted
